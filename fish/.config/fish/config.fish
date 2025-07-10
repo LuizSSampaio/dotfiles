fe1e0f13676c50c3ff631e1715ec9a6af1ab4ee4
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-07-10 08:24:50 -0300

    added pokemon on shell startup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function fish_greeting
+    pokemon-colorscripts -r --no-title
+end
+
 zoxide init fish | source
 starship init fish | source
 
