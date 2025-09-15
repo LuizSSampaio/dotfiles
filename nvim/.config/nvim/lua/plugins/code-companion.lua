9564b7f488b52c08d405574a70ebabd44d749a2f
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-09-15 14:26:33 -0300

    chore(nvim): changed models

--- nvim/.config/nvim/lua/plugins/code-companion.lua
+++ nvim/.config/nvim/lua/plugins/code-companion.lua
@@ -1,88 +1,89 @@
 local default_model = "moonshotai/kimi-k2:free"
 local available_models = {
-	"moonshotai/kimi-k2:free",
-	"openai/gpt-5",
-	"openai/gpt-5-mini",
-	"openai/gpt-5-nano",
-	"anthropic/claude-sonnet-4",
-	"x-ai/grok-4",
+  "moonshotai/kimi-k2:free",
+  "openai/gpt-5",
+  "openai/gpt-5-mini",
+  "openai/gpt-5-nano",
+  "anthropic/claude-sonnet-4",
+  "x-ai/grok-4",
+  "x-ai/grok-code-fast-1",
 }
 local current_model = default_model
 
 local function select_model()
-	vim.ui.select(available_models, {
-		prompt = "Select  Model:",
-	}, function(choice)
-		if choice then
-			current_model = choice
-			vim.notify("Selected model: " .. current_model)
-		end
-	end)
+  vim.ui.select(available_models, {
+    prompt = "Select  Model:",
+  }, function(choice)
+    if choice then
+      current_model = choice
+      vim.notify("Selected model: " .. current_model)
+    end
+  end)
 end
 
 return {
-	"olimorris/codecompanion.nvim",
-	opts = {},
-	config = function()
-		require("codecompanion").setup({
-			strategies = {
-				chat = {
-					adapter = "openrouter",
-				},
-				inline = {
-					adapter = "openrouter",
-				},
-			},
-			adapters = {
-				openrouter = function()
-					return require("codecompanion.adapters").extend("openai_compatible", {
-						env = {
-							url = "https://openrouter.ai/api",
-							api_key = assert(os.getenv("OPENROUTER_API_KEY"), "OPENROUTER_API_KEY env not set"),
-							chat_url = "/v1/chat/completions",
-						},
-						schema = {
-							model = {
-								default = current_model,
-							},
-						},
-					})
-				end,
-			},
-		})
+  "olimorris/codecompanion.nvim",
+  opts = {},
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "openrouter",
+        },
+        inline = {
+          adapter = "openrouter",
+        },
+      },
+      adapters = {
+        openrouter = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://openrouter.ai/api",
+              api_key = assert(os.getenv("OPENROUTER_API_KEY"), "OPENROUTER_API_KEY env not set"),
+              chat_url = "/v1/chat/completions",
+            },
+            schema = {
+              model = {
+                default = current_model,
+              },
+            },
+          })
+        end,
+      },
+    })
 
-		-- Expand 'cc' into 'CodeCompanion' in the command line
-		vim.cmd([[cab cc CodeCompanion]])
-	end,
-	keys = {
-		{ "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
-		{
-			"<leader>aa",
-			"<cmd>CodeCompanionChat Toggle<cr>",
-			desc = "Toggle (CodeCompanion)",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>aq",
-			"<cmd>CodeCompanionChat Add<cr>",
-			desc = "Add to chat (CodeCompanion)",
-			mode = { "v" },
-		},
-		{
-			"<leader>ap",
-			"<cmd>CodeCompanionActions<cr>",
-			desc = "Actions (CodeCompanion)",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>as",
-			select_model,
-			desc = "Select Model (CodeCompanion)",
-			mode = { "n" },
-		},
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
+    -- Expand 'cc' into 'CodeCompanion' in the command line
+    vim.cmd([[cab cc CodeCompanion]])
+  end,
+  keys = {
+    { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+    {
+      "<leader>aa",
+      "<cmd>CodeCompanionChat Toggle<cr>",
+      desc = "Toggle (CodeCompanion)",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>aq",
+      "<cmd>CodeCompanionChat Add<cr>",
+      desc = "Add to chat (CodeCompanion)",
+      mode = { "v" },
+    },
+    {
+      "<leader>ap",
+      "<cmd>CodeCompanionActions<cr>",
+      desc = "Actions (CodeCompanion)",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>as",
+      select_model,
+      desc = "Select Model (CodeCompanion)",
+      mode = { "n" },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
 }
