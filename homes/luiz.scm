f47bc156ada624ac9458b49a151b88c478dca961
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-04-30 21:37:02 -0300

    feat: update nushell config

--- homes/luiz.scm
+++ homes/luiz.scm
@@ -5,6 +5,7 @@
   #:use-module (gnu home services shells)
   #:use-module (gnu home services sound)
   #:use-module (gnu packages nushell)
+  #:use-module (gnu packages rust-apps)
   #:use-module (gnu packages shellutils)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages wget)
@@ -31,11 +32,14 @@
     direnv
 
     ;; CLI utilities
+    bat
     curl
+    eza
     wget
     file
     unzip
     zip
+    zoxide
 
     ;; Gaming
     %steam-package
@@ -53,12 +57,88 @@
 (define %nushell-config-file
   (plain-file
    "config.nu"
-   "$env.config = ($env.config? | default {})
-$env.config = ($env.config | upsert hooks.env_change.PWD [
-  {|before, after|
-    direnv export json | from json | default {} | load-env
+"$env.config = (
+  $env.config?
+  | default {}
+  | upsert history { default {} }
+  | upsert history.max_size 1000
+  | upsert completions { default {} }
+  | upsert completions.external { default {} }
+  | upsert completions.external.enable true
+  | upsert highlight_resolved_externals true
+  | upsert hooks { default {} }
+  | upsert hooks.env_change { default {} }
+  | upsert hooks.env_change.PWD { default [] }
+)
+
+$env.config.hooks.env_change.PWD = ($env.config.hooks.env_change.PWD | append {
+  __direnv_hook: true,
+  code: {|before, after|
+    let direnv_export = (direnv export json)
+    if not ($direnv_export | is-empty) {
+      $direnv_export | from json | default {} | load-env
+    }
   }
-])
+})
+
+source ~/.config/nushell/zoxide.nu
+
+alias c = clear
+alias mkdir = ^mkdir -vp
+alias rm = ^rm -rifv
+alias mv = ^mv -iv
+alias cp = ^cp -riv
+alias cat = ^bat --paging=never --style=plain
+alias cd = z
+alias ls = ^eza -lh --group-directories-first --icons=auto
+alias lsa = ls -a
+alias lt = ^eza --tree --level=2 --long --icons --git
+alias lta = lt -a
+alias n = ^emacs -nw
+"))
+
+(define %zoxide-nushell-file
+  (plain-file
+   "zoxide.nu"
+   "# Code generated from `zoxide init nushell`.
+
+export-env {
+  $env.config = (
+    $env.config?
+    | default {}
+    | upsert hooks { default {} }
+    | upsert hooks.env_change { default {} }
+    | upsert hooks.env_change.PWD { default [] }
+  )
+  let __zoxide_hooked = (
+    $env.config.hooks.env_change.PWD | any { try { get __zoxide_hook } catch { false } }
+  )
+  if not $__zoxide_hooked {
+    $env.config.hooks.env_change.PWD = ($env.config.hooks.env_change.PWD | append {
+      __zoxide_hook: true,
+      code: {|_, dir| ^zoxide add -- $dir}
+    })
+  }
+}
+
+def --env --wrapped __zoxide_z [...rest: string] {
+  let path = match $rest {
+    [] => {'~'},
+    [ '-' ] => {'-'},
+    [ $arg ] if ($arg | path expand | path type) == 'dir' => {$arg}
+    _ => {
+      ^zoxide query --exclude $env.PWD -- ...$rest | str trim -r -c \"\\n\"
+    }
+  }
+  cd $path
+}
+
+def --env --wrapped __zoxide_zi [...rest:string] {
+  cd $'(^zoxide query --interactive -- ...$rest | str trim -r -c \"\\n\")'
+}
+
+alias z = __zoxide_z
+alias zi = __zoxide_zi
 "))
 
 (define %home-services
@@ -68,7 +148,8 @@ $env.config = ($env.config | upsert hooks.env_change.PWD [
      'nushell-config-files
      home-files-service-type
      (list
-      `(".config/nushell/config.nu" ,%nushell-config-file)))
+      `(".config/nushell/config.nu" ,%nushell-config-file)
+      `(".config/nushell/zoxide.nu" ,%zoxide-nushell-file)))
 
     ;; Persist common environment variables across all sessions.
     (service home-environment-variables-service-type
